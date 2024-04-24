9cef4fca5a34d33218845c08657431a25954a6e3
arabi44-cyber <arabiofficial107@gmail.com>
2024-04-24 23:54:13 +0545

    complete

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountID = 12345
+let accountEmail = "arabi107@gmail.com"
+var accountpassword = "23342"
+accountCity = "Butwal"
+
+let  accountstate;
+
+// accountID = 2 // not allowed 
+
+
+accountEmail = "hc@gmail.com"
+accountpassword = "1212"
+accountCity = " kathmandu"
+
+console.log(accountID)
+/*
+prefer not to use var
+because of issue on block scope and functional scope  
+*/
+console.table([ accountID,accountEmail,accountpassword,accountCity,accountstate])
---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,68 @@
+let score = "arabi"
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "arabi"
+
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoogedIn);
+
+
+// 1 => true; 0 => false 
+// "" => false
+// "arabi" => true
+
+let somenumber = 33
+let stringnumber = String(somenumber)
+// console.log(stringnumber)
+// console.log( typeof stringnumber)
+
+
+ // ################## Operation ############
+
+ 
+ let value = 3
+ let negValue =  -value
+
+//  console.log( negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2*2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = " hello"
+let str2 = " arabi"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log(1 +"2")
+// console.log("1"+ "2" )
+// coonsole.log("1"+ 2 + 2 )
+// coonsole.log(1 + 2 + "2" )
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+ 
+
+num1 = num2 = num3 = 2+3
+ let gameCounter =  100
+ ++gameCounter;
+ console.log(gameCounter)
