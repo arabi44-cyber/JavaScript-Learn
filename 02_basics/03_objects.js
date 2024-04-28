de19e2dd6bdada37ec3355450314339bff770147
arabi44-cyber <arabiofficial107@gmail.com>
2024-04-28 08:52:05 +0545

    Objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,36 @@
+// singleton
+
+// Object.create constructor 
+// objects literals
+const mySym = Symbol("key1")
+
+const jsUser = {
+    name: "Rabi Basnet",
+    [mySym]: "mykey1",
+    address: "ktm",
+    age: 18,
+    email: "learner.soul11@gmail.com",
+    isLoggenIN: false,
+    lastLoginday: ["Monday","Sunday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
+console.log(jsUser[mySym])
+
+
+jsUser.email = "arabi107.com"
+// Object.freeze(jsUser)
+jsUser.email = "arabi107google.com"
+console.log(jsUser);
+
+ 
+jsUser.greeting = function(){
+    console.log("Hello Js User");
+}
+jsUser.greetingTwo = function(){
+    console.log(`Hello Js User, ${this.name}`);
+}
+
+console.log(jsUser.greeting());
+console.log(jsUser.greetingTwo());
