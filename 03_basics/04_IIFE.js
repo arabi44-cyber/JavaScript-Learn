85554d3072abb5283dff5c7b01a58ca9cb4d5a4b
arabi44-cyber <arabiofficial107@gmail.com>
2024-05-06 02:04:41 +0545

    IIFI

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,15 @@
+// Immediately Invoked Function Expressions (IFFE)
+
+(function chai (){
+
+    // named IIFI
+    console.log(`DB CONNECTED`)
+
+})();
+
+(  (name) => {
+
+    // unnamed IIFI
+    console.log(`DB CONNECTED TWO ${name}`);
+}) ('King')
+
