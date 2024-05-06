0a88fac32d9567fb3b29d82a0076c2a6e183b10e
arabi44-cyber <arabiofficial107@gmail.com>
2024-05-06 09:35:01 +0545

    break, deafult in switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+const month = "March"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("Feb");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+
+    default:
+        console.log ( "deafult case match")
+        break;
+}
