e6ee41e5f9dd0e60ff68ad23fe49438d6d3f8f85
arabi44-cyber <arabiofficial107@gmail.com>
2024-05-06 09:34:30 +0545

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+// if 
+
+// const isUserloggedIn = true
+
+// const temperature = 41
+
+// if(temperature === 40 ){
+// console.log("Less than 50");
+
+// } else {
+
+// console.log("temperature is greater than 50");
+// }
+
+// console.log("execute")
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 200 
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+
+// console.log(`User power:${power}`);
+
+const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log( "test2");
+
+// if (balance < 500) {
+//     console.log("less than")
+
+// } else if(balance < 750){
+//     console.log("les tahn 750")
+// }else if ( balance < 900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+
+const  isUserloggedIn  = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedFromEmail = true
+
+if (isUserloggedIn && debitcard && 2 == 3) {
+    console.log("Allow to Buy Course")
+    
+}
+
+if (loggedFromEmail || loggedInFromGoogle) {
+    console.log("User Logged in")
+    
+}
+
