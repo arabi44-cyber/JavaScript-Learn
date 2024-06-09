3c4c552f3579f14dacb3670fa5a245b5172f124d
arabi44-cyber <arabiofficial107@gmail.com>
2024-06-09 21:46:23 +0545

    filter

--- 05_iteration/6Six.js
+++ 05_iteration/6Six.js
@@ -12,7 +12,7 @@ const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const newNums = myNums.filter(  (num) => {
 //     return num > 4
 // } )
-
+console.log('arabi44-cyber');
 const newNums = [];
 myNums.forEach((num) => {
   if (num > 4) {
